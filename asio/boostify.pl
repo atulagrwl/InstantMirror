85cf5e507c68105d8015a689ac4d147453bc2e35
Atul Aggarwal <searchatul@gmail.com>
2009-06-28 01:21:41 +0530

    Added asio src as required by libtorrent

---
+++ asio/boostify.pl
@@ -0,0 +1,648 @@
+#!/usr/bin/perl -w
+
+use strict;
+use File::Path;
+
+our $boost_dir = "boostified";
+
+sub print_line
+{
+  my ($output, $line, $from, $lineno) = @_;
+
+  # Warn if the resulting line is >80 characters wide.
+  if (length($line) > 80)
+  {
+    if ($from =~ /\.[chi]pp$/)
+    {
+      print("Warning: $from:$lineno: output >80 characters wide.\n");
+    }
+  }
+
+  # Write the output.
+  print($output $line . "\n");
+}
+
+sub source_contains_asio_thread_usage
+{
+  my ($from) = @_;
+
+  # Open the input file.
+  open(my $input, "<$from") or die("Can't open $from for reading");
+
+  # Check file for use of asio::thread.
+  while (my $line = <$input>)
+  {
+    chomp($line);
+    if ($line =~ /asio::thread/)
+    {
+      close($input);
+      return 1;
+    }
+    elsif ($line =~ /^ *thread /)
+    {
+      close($input);
+      return 1;
+    }
+  }
+
+  close($input);
+  return 0;
+}
+
+sub source_contains_asio_include
+{
+  my ($from) = @_;
+
+  # Open the input file.
+  open(my $input, "<$from") or die("Can't open $from for reading");
+
+  # Check file for inclusion of asio.hpp.
+  while (my $line = <$input>)
+  {
+    chomp($line);
+    if ($line =~ /# *include [<"]asio\.hpp[>"]/)
+    {
+      close($input);
+      return 1;
+    }
+  }
+
+  close($input);
+  return 0;
+}
+
+sub source_contains_asio_error_code_include
+{
+  my ($from) = @_;
+
+  # Open the input file.
+  open(my $input, "<$from") or die("Can't open $from for reading");
+
+  # Check file for inclusion of asio/error_code.hpp.
+  while (my $line = <$input>)
+  {
+    chomp($line);
+    if ($line =~ /# *include [<"]asio\/error_code\.hpp[>"]/)
+    {
+      close($input);
+      return 1;
+    }
+  }
+
+  close($input);
+  return 0;
+}
+
+sub source_contains_asio_system_error_include
+{
+  my ($from) = @_;
+
+  # Open the input file.
+  open(my $input, "<$from") or die("Can't open $from for reading");
+
+  # Check file for inclusion of asio/system_error.hpp.
+  while (my $line = <$input>)
+  {
+    chomp($line);
+    if ($line =~ /# *include [<"]asio\/system_error\.hpp[>"]/)
+    {
+      close($input);
+      return 1;
+    }
+  }
+
+  close($input);
+  return 0;
+}
+
+sub source_contains_boostify_error_categories
+{
+  my ($from) = @_;
+
+  # Open the input file.
+  open(my $input, "<$from") or die("Can't open $from for reading");
+
+  # Check file for boostify error category directive.
+  while (my $line = <$input>)
+  {
+    chomp($line);
+    if ($line =~ /boostify: error category/)
+    {
+      close($input);
+      return 1;
+    }
+  }
+
+  close($input);
+  return 0;
+}
+
+my $error_cat_defns = <<"EOF";
+inline const boost::system::error_category& get_system_category()
+{
+  return boost::system::get_system_category();
+}
+
+#if !defined(BOOST_WINDOWS) && !defined(__CYGWIN__)
+
+namespace detail {
+
+class netdb_category : public boost::system::error_category
+{
+public:
+  const char* name() const
+  {
+    return "asio.netdb";
+  }
+
+  std::string message(int value) const
+  {
+    if (value == error::host_not_found)
+      return "Host not found (authoritative)";
+    if (value == error::host_not_found_try_again)
+      return "Host not found (non-authoritative), try again later";
+    if (value == error::no_data)
+      return "The query is valid, but it does not have associated data";
+    if (value == error::no_recovery)
+      return "A non-recoverable error occurred during database lookup";
+    return "asio.netdb error";
+  }
+};
+
+} // namespace detail
+
+inline const boost::system::error_category& get_netdb_category()
+{
+  static detail::netdb_category instance;
+  return instance;
+}
+
+namespace detail {
+
+class addrinfo_category : public boost::system::error_category
+{
+public:
+  const char* name() const
+  {
+    return "asio.addrinfo";
+  }
+
+  std::string message(int value) const
+  {
+    if (value == error::service_not_found)
+      return "Service not found";
+    if (value == error::socket_type_not_supported)
+      return "Socket type not supported";
+    return "asio.addrinfo error";
+  }
+};
+
+} // namespace detail
+
+inline const boost::system::error_category& get_addrinfo_category()
+{
+  static detail::addrinfo_category instance;
+  return instance;
+}
+
+#else // !defined(BOOST_WINDOWS) && !defined(__CYGWIN__)
+
+inline const boost::system::error_category& get_netdb_category()
+{
+  return get_system_category();
+}
+
+inline const boost::system::error_category& get_addrinfo_category()
+{
+  return get_system_category();
+}
+
+#endif // !defined(BOOST_WINDOWS) && !defined(__CYGWIN__)
+
+namespace detail {
+
+class misc_category : public boost::system::error_category
+{
+public:
+  const char* name() const
+  {
+    return "asio.misc";
+  }
+
+  std::string message(int value) const
+  {
+    if (value == error::already_open)
+      return "Already open";
+    if (value == error::eof)
+      return "End of file";
+    if (value == error::not_found)
+      return "Element not found";
+    if (value == error::fd_set_failure)
+      return "The descriptor does not fit into the select call's fd_set";
+    return "asio.misc error";
+  }
+};
+
+} // namespace detail
+
+inline const boost::system::error_category& get_misc_category()
+{
+  static detail::misc_category instance;
+  return instance;
+}
+
+namespace detail {
+
+class ssl_category : public boost::system::error_category
+{
+public:
+  const char* name() const
+  {
+    return "asio.ssl";
+  }
+
+  std::string message(int) const
+  {
+    return "asio.ssl error";
+  }
+};
+
+} // namespace detail
+
+inline const boost::system::error_category& get_ssl_category()
+{
+  static detail::ssl_category instance;
+  return instance;
+}
+
+static const boost::system::error_category& system_category
+  = boost::asio::error::get_system_category();
+static const boost::system::error_category& netdb_category
+  = boost::asio::error::get_netdb_category();
+static const boost::system::error_category& addrinfo_category
+  = boost::asio::error::get_addrinfo_category();
+static const boost::system::error_category& misc_category
+  = boost::asio::error::get_misc_category();
+static const boost::system::error_category& ssl_category
+  = boost::asio::error::get_ssl_category();
+
+} // namespace error
+} // namespace asio
+
+namespace system {
+
+template<> struct is_error_code_enum<boost::asio::error::basic_errors>
+{
+  static const bool value = true;
+};
+
+template<> struct is_error_code_enum<boost::asio::error::netdb_errors>
+{
+  static const bool value = true;
+};
+
+template<> struct is_error_code_enum<boost::asio::error::addrinfo_errors>
+{
+  static const bool value = true;
+};
+
+template<> struct is_error_code_enum<boost::asio::error::misc_errors>
+{
+  static const bool value = true;
+};
+
+template<> struct is_error_code_enum<boost::asio::error::ssl_errors>
+{
+  static const bool value = true;
+};
+
+} // namespace system
+
+namespace asio {
+namespace error {
+EOF
+
+sub copy_source_file
+{
+  my ($from, $to) = @_;
+
+  # Ensure the output directory exists.
+  my $dir = $to;
+  $dir =~ s/[^\/]*$//;
+  mkpath($dir);
+
+  # First determine whether the file makes any use of asio::thread.
+  my $uses_asio_thread = source_contains_asio_thread_usage($from);
+  my $includes_asio = source_contains_asio_include($from);
+
+  # Check whether the file includes error handling header files.
+  my $includes_error_code = source_contains_asio_error_code_include($from);
+  my $includes_system_error = source_contains_asio_system_error_include($from);
+  my $includes_boostify_ecats = source_contains_boostify_error_categories($from);
+
+  # Open the files.
+  open(my $input, "<$from") or die("Can't open $from for reading");
+  open(my $output, ">$to") or die("Can't open $to for writing");
+
+  # Copy the content.
+  my $lineno = 1;
+  while (my $line = <$input>)
+  {
+    chomp($line);
+
+    # Unconditional replacements.
+    $line =~ s/[\\@]ref boost_bind/boost::bind()/g;
+    if ($from =~ /.*\.txt$/)
+    {
+      $line =~ s/[\\@]ref async_read/boost::asio::async_read()/g;
+      $line =~ s/[\\@]ref async_write/boost::asio::async_write()/g;
+    }
+
+    # Conditional replacements.
+    if ($line =~ /^namespace asio {/)
+    {
+      print_line($output, "namespace boost {", $from, $lineno);
+      print_line($output, $line, $from, $lineno);
+    }
+    elsif ($line =~ /^} \/\/ namespace asio$/)
+    {
+      print_line($output, $line, $from, $lineno);
+      print_line($output, "} // namespace boost", $from, $lineno);
+    }
+    elsif ($line =~ /^(# *include )[<"](asio\.hpp)[>"]$/)
+    {
+      print_line($output, $1 . "<boost/" . $2 . ">", $from, $lineno);
+      if ($uses_asio_thread)
+      {
+        print_line($output, $1 . "<boost/thread.hpp>", $from, $lineno);
+        $uses_asio_thread = 0;
+      }
+    }
+    elsif ($line =~ /^(# *include )[<"]boost\/.*[>"].*$/)
+    {
+      if (!$includes_asio && $uses_asio_thread)
+      {
+        print_line($output, $1 . "<boost/thread.hpp>", $from, $lineno);
+        $uses_asio_thread = 0;
+      }
+      print_line($output, $line, $from, $lineno);
+    }
+    elsif ($line =~ /^(# *include )[<"](asio\/detail\/pop_options\.hpp)[>"]$/)
+    {
+      if ($includes_boostify_ecats)
+      {
+        $includes_boostify_ecats = 0;
+        print_line($output, $1 . "<boost/cerrno.hpp>", $from, $lineno);
+      }
+      if ($includes_error_code)
+      {
+        $includes_error_code = 0;
+        print_line($output, $1 . "<boost/system/error_code.hpp>", $from, $lineno);
+      }
+      if ($includes_system_error)
+      {
+        $includes_system_error = 0;
+        print_line($output, $1 . "<boost/system/system_error.hpp>", $from, $lineno);
+      }
+      print_line($output, $1 . "<boost/" . $2 . ">", $from, $lineno);
+    }
+    elsif ($line =~ /# *include <cerrno>/)
+    {
+      if ($includes_boostify_ecats)
+      {
+        # Line is removed.
+      }
+      else
+      {
+        print_line($output, $line, $from, $lineno);
+      }
+    }
+    elsif ($line =~ /# *include [<"]asio\/thread\.hpp[>"]/)
+    {
+      # Line is removed.
+    }
+    elsif ($line =~ /# *include [<"]asio\/error_code\.hpp[>"]/)
+    {
+      # Line is removed.
+    }
+    elsif ($line =~ /# *include [<"]asio\/impl\/error_code\.ipp[>"]/)
+    {
+      # Line is removed.
+    }
+    elsif ($line =~ /# *include [<"]asio\/system_error\.hpp[>"]/)
+    {
+      # Line is removed.
+    }
+    elsif ($line =~ /^(# *include )[<"](asio\/.*)[>"](.*)$/)
+    {
+      print_line($output, $1 . "<boost/" . $2 . ">" . $3, $from, $lineno);
+    }
+    elsif ($line =~ /ASIO_/ && !($line =~ /BOOST_ASIO_/))
+    {
+      $line =~ s/ASIO_/BOOST_ASIO_/g;
+      if ($line =~ /asio::error_code/)
+      {
+        $line =~ s/asio::error_code/boost::system::error_code/g;
+      }
+      print_line($output, $line, $from, $lineno);
+    }
+    elsif ($line =~ /asio::thread/)
+    {
+      $line =~ s/asio::thread/boost::thread/g;
+      if (!($line =~ /boost::asio::/))
+      {
+        $line =~ s/asio::/boost::asio::/g;
+      }
+      print_line($output, $line, $from, $lineno);
+    }
+    elsif ($line =~ /^( *)thread( .*)$/)
+    {
+      if (!($line =~ /boost::asio::/))
+      {
+        $line =~ s/asio::/boost::asio::/g;
+      }
+      print_line($output, $1 . "boost::thread" . $2, $from, $lineno);
+    }
+    elsif ($line =~ /boostify: error category definitions go here/)
+    {
+      print($output $error_cat_defns);
+    }
+    elsif ($line =~ /asio::/ && !($line =~ /boost::asio::/))
+    {
+      $line =~ s/asio::error_code/boost::system::error_code/g;
+      $line =~ s/asio::system_error/boost::system::system_error/g;
+      $line =~ s/asio::/boost::asio::/g;
+      print_line($output, $line, $from, $lineno);
+    }
+    elsif ($line =~ /using namespace asio/)
+    {
+      $line =~ s/using namespace asio/using namespace boost::asio/g;
+      print_line($output, $line, $from, $lineno);
+    }
+    elsif ($line =~ /asio_handler_alloc_helpers/)
+    {
+      $line =~ s/asio_handler_alloc_helpers/boost_asio_handler_alloc_helpers/g;
+      print_line($output, $line, $from, $lineno);
+    }
+    elsif ($line =~ /asio_handler_invoke_helpers/)
+    {
+      $line =~ s/asio_handler_invoke_helpers/boost_asio_handler_invoke_helpers/g;
+      print_line($output, $line, $from, $lineno);
+    }
+    elsif ($line =~ /[\\@]ref boost_bind/)
+    {
+      $line =~ s/[\\@]ref boost_bind/boost::bind()/g;
+      print_line($output, $line, $from, $lineno);
+    }
+    else
+    {
+      print_line($output, $line, $from, $lineno);
+    }
+    ++$lineno;
+  }
+
+  # Ok, we're done.
+  close($input);
+  close($output);
+}
+
+sub copy_include_files
+{
+  my @dirs = (
+      "include",
+      "include/asio",
+      "include/asio/detail",
+      "include/asio/impl",
+      "include/asio/ip",
+      "include/asio/ip/detail",
+      "include/asio/local",
+      "include/asio/posix",
+      "include/asio/ssl",
+      "include/asio/ssl/detail",
+      "include/asio/windows");
+
+  foreach my $dir (@dirs)
+  {
+    our $boost_dir;
+    my @files = ( glob("$dir/*.hpp"), glob("$dir/*.ipp") );
+    foreach my $file (@files)
+    {
+      if ($file ne "include/asio/thread.hpp"
+          and $file ne "include/asio/error_code.hpp"
+          and $file ne "include/asio/system_error.hpp"
+          and $file ne "include/asio/impl/error_code.ipp")
+      {
+        my $from = $file;
+        my $to = $file;
+        $to =~ s/^include\//$boost_dir\/boost\//;
+        copy_source_file($from, $to);
+      }
+    }
+  }
+}
+
+sub create_lib_directory
+{
+  my @dirs = (
+      "doc",
+      "example",
+      "test");
+
+  our $boost_dir;
+  foreach my $dir (@dirs)
+  {
+    mkpath("$boost_dir/libs/asio/$dir");
+  }
+}
+
+sub copy_unit_tests
+{
+  my @dirs = (
+      "src/tests/unit",
+      "src/tests/unit/ip",
+      "src/tests/unit/local",
+      "src/tests/unit/posix",
+      "src/tests/unit/ssl",
+      "src/tests/unit/windows");
+
+  our $boost_dir;
+  foreach my $dir (@dirs)
+  {
+    my @files = ( glob("$dir/*.*pp"), glob("$dir/Jamfile*") );
+    foreach my $file (@files)
+    {
+      if ($file ne "src/tests/unit/thread.cpp"
+          and $file ne "src/tests/unit/error_handler.cpp")
+      {
+        my $from = $file;
+        my $to = $file;
+        $to =~ s/^src\/tests\/unit\//$boost_dir\/libs\/asio\/test\//;
+        copy_source_file($from, $to);
+      }
+    }
+  }
+}
+
+sub copy_examples
+{
+  my @dirs = (
+      "src/examples/allocation",
+      "src/examples/buffers",
+      "src/examples/chat",
+      "src/examples/echo",
+      "src/examples/http/client",
+      "src/examples/http/doc_root",
+      "src/examples/http/server",
+      "src/examples/http/server2",
+      "src/examples/http/server3",
+      "src/examples/invocation",
+      "src/examples/iostreams",
+      "src/examples/local",
+      "src/examples/multicast",
+      "src/examples/nonblocking",
+      "src/examples/porthopper",
+      "src/examples/serialization",
+      "src/examples/services",
+      "src/examples/socks4",
+      "src/examples/ssl",
+      "src/examples/timeouts",
+      "src/examples/timers",
+      "src/examples/tutorial",
+      "src/examples/tutorial/daytime1",
+      "src/examples/tutorial/daytime2",
+      "src/examples/tutorial/daytime3",
+      "src/examples/tutorial/daytime4",
+      "src/examples/tutorial/daytime5",
+      "src/examples/tutorial/daytime6",
+      "src/examples/tutorial/daytime7",
+      "src/examples/tutorial/timer1",
+      "src/examples/tutorial/timer2",
+      "src/examples/tutorial/timer3",
+      "src/examples/tutorial/timer4",
+      "src/examples/tutorial/timer5",
+      "src/examples/windows");
+
+  our $boost_dir;
+  foreach my $dir (@dirs)
+  {
+    my @files = (
+        glob("$dir/*.*pp"),
+        glob("$dir/*.html"),
+        glob("$dir/Jamfile*"),
+        glob("$dir/*.pem"),
+        glob("$dir/README*"),
+        glob("$dir/*.txt"));
+    foreach my $file (@files)
+    {
+      my $from = $file;
+      my $to = $file;
+      $to =~ s/^src\/examples\//$boost_dir\/libs\/asio\/example\//;
+      copy_source_file($from, $to);
+    }
+  }
+}
+
+copy_include_files();
+create_lib_directory();
+copy_unit_tests();
+copy_examples();
---
+++ asio/release.pl
@@ -0,0 +1,332 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Cwd qw(abs_path getcwd);
+use Date::Format;
+use File::Path;
+use File::Copy;
+
+our $version_major;
+our $version_minor;
+our $version_sub_minor;
+our $asio_name;
+our $boost_asio_name;
+
+sub print_usage_and_exit
+{
+  print("Usage: ./release.pl <version>\n");
+  print("  Example: ./release.pl 1.2.0\n");
+  exit(1);
+}
+
+sub determine_version($)
+{
+  my $version_string = shift;
+  if ($version_string =~ /^([0-9]+)\.([0-9]+)\.([0-9]+)$/)
+  {
+    our $version_major = $1;
+    our $version_minor = $2;
+    our $version_sub_minor = $3;
+
+    our $asio_name = "asio";
+    $asio_name .= "-$version_major";
+    $asio_name .= ".$version_minor";
+    $asio_name .= ".$version_sub_minor";
+
+    our $boost_asio_name = "boost_asio";
+    $boost_asio_name .= "_$version_major";
+    $boost_asio_name .= "_$version_minor";
+    $boost_asio_name .= "_$version_sub_minor";
+  }
+  else
+  {
+    print_usage_and_exit();
+  }
+}
+
+sub update_configure_ac
+{
+  # Open the files.
+  my $from = "configure.ac";
+  my $to = $from . ".tmp";
+  open(my $input, "<$from") or die("Can't open $from for reading");
+  open(my $output, ">$to") or die("Can't open $to for writing");
+
+  # Copy the content.
+  while (my $line = <$input>)
+  {
+    chomp($line);
+    if ($line =~ /^AC_INIT\(asio.*\)$/)
+    {
+      $line = "AC_INIT(asio, [";
+      $line .= "$version_major.$version_minor.$version_sub_minor";
+      $line .= "])";
+    }
+    print($output "$line\n");
+  }
+
+  # Close the files and move the temporary output into position.
+  close($input);
+  close($output);
+  move($to, $from);
+  unlink($to);
+}
+
+sub update_readme
+{
+  # Open the files.
+  my $from = "README";
+  my $to = $from . ".tmp";
+  open(my $input, "<$from") or die("Can't open $from for reading");
+  open(my $output, ">$to") or die("Can't open $to for writing");
+
+  # Copy the content.
+  while (my $line = <$input>)
+  {
+    chomp($line);
+    if ($line =~ /^asio version/)
+    {
+      $line = "asio version ";
+      $line .= "$version_major.$version_minor.$version_sub_minor";
+    }
+    elsif ($line =~ /^Released/)
+    {
+      my @time = localtime;
+      $line = "Released " . strftime("%A, %d %B %Y", @time) . ".";
+    }
+    print($output "$line\n");
+  }
+
+  # Close the files and move the temporary output into position.
+  close($input);
+  close($output);
+  move($to, $from);
+  unlink($to);
+}
+
+sub update_asio_version_hpp
+{
+  # Open the files.
+  my $from = "include/asio/version.hpp";
+  my $to = $from . ".tmp";
+  open(my $input, "<$from") or die("Can't open $from for reading");
+  open(my $output, ">$to") or die("Can't open $to for writing");
+
+  # Copy the content.
+  while (my $line = <$input>)
+  {
+    chomp($line);
+    if ($line =~ /^#define ASIO_VERSION /)
+    {
+      my $version = $version_major * 100000;
+      $version += $version_minor * 100;
+      $version += $version_sub_minor + 0;
+      $line = "#define ASIO_VERSION " . $version;
+      $line .= " // $version_major.$version_minor.$version_sub_minor";
+    }
+    print($output "$line\n");
+  }
+
+  # Close the files and move the temporary output into position.
+  close($input);
+  close($output);
+  move($to, $from);
+  unlink($to);
+}
+
+sub update_boost_asio_version_hpp
+{
+  # Open the files.
+  my $from = "../boost/boost/asio/version.hpp";
+  my $to = $from . ".tmp";
+  open(my $input, "<$from") or die("Can't open $from for reading");
+  open(my $output, ">$to") or die("Can't open $to for writing");
+
+  # Copy the content.
+  while (my $line = <$input>)
+  {
+    chomp($line);
+    if ($line =~ /^#define BOOST_ASIO_VERSION /)
+    {
+      my $version = $version_major * 100000;
+      $version += $version_minor * 100;
+      $version += $version_sub_minor + 0;
+      $line = "#define BOOST_ASIO_VERSION " . $version;
+      $line .= " // $version_major.$version_minor.$version_sub_minor";
+    }
+    print($output "$line\n");
+  }
+
+  # Close the files and move the temporary output into position.
+  close($input);
+  close($output);
+  move($to, $from);
+  unlink($to);
+}
+
+sub build_asio_doc
+{
+  $ENV{BOOST_ROOT} = abs_path("../boost");
+  system("rm -rf doc");
+  my $bjam = abs_path(glob("../boost/tools/jam/src/bin.*/bjam"));
+  chdir("src/doc");
+  system("$bjam clean");
+  system("rm -rf html");
+  system("$bjam");
+  chdir("../..");
+  mkdir("doc");
+  system("cp -vR src/doc/html/* doc");
+}
+
+sub make_asio_packages
+{
+  system("./autogen.sh");
+  system("./configure");
+  system("make dist");
+}
+
+sub build_boost_asio_doc
+{
+  my $cwd = getcwd();
+  my $bjam = abs_path(glob("../boost/tools/jam/src/bin.*/bjam"));
+  chdir("../boost/doc");
+  system("$bjam clean");
+  system("rm -rf html/boost_asio");
+  system("$bjam asio");
+  chdir($cwd);
+}
+
+our $boost_asio_readme = <<"EOF";
+Copy the `boost', `doc' and `libs' directories into an existing boost 1.33.0,
+1.33.1, 1.34, 1.34.1, 1.35 or 1.36 distribution.
+
+Before using Boost.Asio, the Boost.System library needs to be built. This can
+be done by running bjam in the libs/system/build directory. Consult the Boost
+Getting Started page (http://www.boost.org/more/getting_started.html) for more
+information on how to build the Boost libraries.
+EOF
+
+our $boost_system_jamfile = <<"EOF";
+# Boost System Library Build Jamfile
+
+# (C) Copyright Beman Dawes 2002, 2006
+
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or www.boost.org/LICENSE_1_0.txt)
+
+# See library home page at http://www.boost.org/libs/system
+
+subproject libs/system/build ;
+
+SOURCES = error_code ;
+
+lib boost_system
+     : ../src/$(SOURCES).cpp
+     : # build requirements
+      <define>BOOST_SYSTEM_STATIC_LINK
+      <sysinclude>$(BOOST_AUX_ROOT) <sysinclude>$(BOOST_ROOT)
+      # common-variant-tag ensures that the library will
+      # be named according to the rules used by the install
+      # and auto-link features:
+      common-variant-tag 
+     : debug release  # build variants
+     ;
+
+dll boost_system
+     : ../src/$(SOURCES).cpp
+     : # build requirements
+       <define>BOOST_SYSTEM_DYN_LINK=1  # tell source we're building dll's
+       <runtime-link>dynamic  # build only for dynamic runtimes
+       <sysinclude>$(BOOST_AUX_ROOT) <sysinclude>$(BOOST_ROOT)
+      # common-variant-tag ensures that the library will
+      # be named according to the rules used by the install
+      # and auto-link features:
+      common-variant-tag 
+     : debug release  # build variants
+     ;
+
+install system lib
+     : <lib>boost_system <dll>boost_system
+     ;
+
+stage stage/lib : <lib>boost_system <dll>boost_system
+    :
+        # copy to a path rooted at BOOST_ROOT:
+        <locate>$(BOOST_ROOT)
+        # make sure the names of the libraries are correctly named:
+        common-variant-tag
+        # add this target to the "stage" and "all" psuedo-targets:
+        <target>stage
+        <target>all
+    :
+        debug release
+    ;
+
+# end
+EOF
+
+sub create_boost_asio_content
+{
+  # Create directory structure.
+  system("rm -rf $boost_asio_name");
+  mkdir("$boost_asio_name");
+  mkdir("$boost_asio_name/doc");
+  mkdir("$boost_asio_name/doc/html");
+  mkdir("$boost_asio_name/boost");
+  mkdir("$boost_asio_name/boost/config");
+  mkdir("$boost_asio_name/libs");
+
+  # Copy files.
+  system("cp -vR ../boost/doc/html/boost_asio.html $boost_asio_name/doc/html");
+  system("cp -vR ../boost/doc/html/boost_asio $boost_asio_name/doc/html");
+  system("cp -vR ../boost/boost/asio.hpp $boost_asio_name/boost");
+  system("cp -vR ../boost/boost/asio $boost_asio_name/boost");
+  system("cp -vR ../boost/boost/cerrno.hpp $boost_asio_name/boost");
+  system("cp -vR ../boost/boost/config/warning_disable.hpp $boost_asio_name/boost/config");
+  system("cp -vR ../boost/boost/system $boost_asio_name/boost");
+  system("cp -vR ../boost/libs/asio $boost_asio_name/libs");
+  system("cp -vR ../boost/libs/system $boost_asio_name/libs");
+
+  # Create readme.
+  my $to = "$boost_asio_name/README.txt";
+  open(my $output, ">$to") or die("Can't open $to for writing");
+  print($output $boost_asio_readme);
+  close($output);
+
+  # Create Boost.System Jamfile.
+  $to = "$boost_asio_name/libs/system/build/Jamfile";
+  open($output, ">$to") or die("Can't open $to for writing");
+  print($output $boost_system_jamfile);
+  close($output);
+
+  # Remove SVN files.
+  system("find $boost_asio_name -name .svn -exec rm -rf {} \\;");
+}
+
+sub make_boost_asio_packages
+{
+  system("tar --format=ustar -chf - $boost_asio_name | gzip -c >$boost_asio_name.tar.gz");
+  system("tar --format=ustar -chf - $boost_asio_name | bzip2 -9 -c >$boost_asio_name.tar.bz2");
+  system("rm -f $boost_asio_name.zip");
+  system("zip -rq $boost_asio_name.zip $boost_asio_name");
+  system("rm -rf $boost_asio_name");
+}
+
+sub create_manifests
+{
+  system("tar tfz $asio_name.tar.gz | sed -e 's/^[^\\/]*//' | sort > asio.manifest");
+  system("tar tfz $boost_asio_name.tar.gz | sed -e 's/^[^\\/]*//' | sort > boost_asio.manifest");
+}
+
+(scalar(@ARGV) == 1) or print_usage_and_exit();
+determine_version($ARGV[0]);
+update_configure_ac();
+update_readme();
+update_asio_version_hpp();
+update_boost_asio_version_hpp();
+build_asio_doc();
+make_asio_packages();
+build_boost_asio_doc();
+create_boost_asio_content();
+make_boost_asio_packages();
+create_manifests();
